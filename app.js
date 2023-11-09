9a092ae3c88f32497521f6a4d5ab8d1488ba0de8
Han Thar <mikhinhantharsoe@uit.edu.mm>
2023-11-09 21:22:30 +0630

    Javascript abc key clickable

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ let keyPiano ={
     '5': 'audio/5.mp3',
     '6': 'audio/6.mp3',
     '7': 'audio/7.mp3',
-    '8': 'audio/1.mp3',
-    '9': 'audio/2.mp3',
-    '0': 'audio/3.mp3',
-    'a': 'audio/4.mp3',
-    'b': 'audio/5.mp3',
-    'c': 'audio/6.mp3',
-    'd': 'audio/7.mp3',
-    'e': 'audio/1.mp3',
-    'f': 'audio/2.mp3',
-    'g': 'audio/3.mp3',
-}
+    '8': 'audio/8.mp3',
+    '9': 'audio/9.mp3',
+    '0': 'audio/10.mp3',
+    'a': 'audio/a.mp3',
+    'b': 'audio/b.mp3',
+    'c': 'audio/c.mp3',
+    'd': 'audio/d.mp3',
+    'e': 'audio/e.mp3',
+    'f': 'audio/f.mp3',
+    'g': 'audio/g.mp3',
+};
 document.addEventListener('keydown', (event) => playPiano(event.key));
 
 function playPiano(key){
@@ -25,13 +25,14 @@ function playPiano(key){
     let keyClick = document.querySelector(`[data-key="${key}"]`);
     keyClick.classList.add('active');
     const removeActive = setInterval(() => {
-        keyClick.classList.remove('active')
-    },500);
+        keyClick.classList.remove('active');
+    }, 500);
 }
-let keys = document.querySelector('.key');
+
+let keys = document.querySelectorAll('.key');
 keys.forEach(key => {
     key.addEventListener('click' , (event) =>{
         let keyClick = key.dataset.key;
         playPiano(keyClick);
-    } )
+    });
 });
